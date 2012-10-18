349c1aaa622e2c3e0316711c6473265e923e5c51
Dmitry Podgorny <pasis.ua@gmail.com>
2012-10-18 23:58:57 +0300

    introduce api for general lists

---
+++ src/list.c
@@ -0,0 +1,62 @@
+/* list.c
+ * libcouplet - fork of libstrophe, XMPP client library
+ * General list routines
+ *
+ * Copyright (C) 2012 Dmitry Podgorny
+ *
+ * This software is provided AS-IS with no warranty
+ *
+ * You can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version
+ * 3 of the License, or any later version.
+ */
+
+/** @file
+ *  General list routines
+ */
+
+#include "list.h"
+
+list_head_t *list_init(const xmpp_ctx_t * const ctx)
+{
+}
+
+list_t *list_init_item(const xmpp_ctx_t * const ctx)
+{
+}
+
+list_t *list_get_first(const list_head_t * const list)
+{
+}
+
+list_t *list_get_next(const list_head_t * const list, const list_t * const item)
+{
+}
+
+list_t *list_get_last(const list_head_t * const list)
+{
+}
+
+list_t *list_pull_first(const list_head_t * const list)
+{
+}
+
+list_t *list_pull_last(const list_head_t * const list)
+{
+}
+
+void list_insert(list_head_t * const list, list_t * const item)
+{
+}
+
+void list_insert_after(list_head_t * const list, list_t * const item, list_t * const after)
+{
+}
+
+void list_append(list_head_t * const list, list_t * const item)
+{
+}
+
+void list_update(list_head_t * const list, list_t * const item, const void * const data)
+{
+}
